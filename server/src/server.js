ec27a69de71405df7817f5eba3a78e3b81fd94bd
IM-Agents <n8n@identixweb.in>
2026-05-02 14:20:27 +0530

    remov log

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 
 const PORT = Number(process.env.PORT) || 3001;
-// test testm2
+
 app.listen(PORT, () => {
   console.log(`Calculator API listening on http://localhost:${PORT}, ${process.env.NODE_ENV}`);
 });
