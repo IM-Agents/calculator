b61150f5235ad4222c933b4300174c95a7f3d9f6
Darshan Chauhan <142311502+iw0108@users.noreply.github.com>
2026-05-13 16:55:13 +0530

    Fix: add new file

---
+++ frontend/src/Demo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const items = ['alpha', 'beta', 'gamma'];
+
+export default function Demo() {
+  console.log('Demo render');
+  const unusedImportPlaceholder = items.length;
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <li style={{ color: 'red' }}>{item}</li>
+      ))}
+    </ul>
+  );
+}
